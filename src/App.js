989705c66f0a5b08bc3a7d9221834dc6261dd73d
chandanikothiya <kothiyachandani34@gmail.com>
2025-11-29 11:31:41 +0530

    learn git

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React & learn git
         </a>
       </header>
     </div>
